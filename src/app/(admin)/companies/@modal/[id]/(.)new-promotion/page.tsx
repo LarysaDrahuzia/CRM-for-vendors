d48898b2a1ada22dfed6c3c40df89593e2702fac
Larysa Drahuzia <draglara@ukr.net>
2025-10-19 20:20:05 +0300

    fix pages

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import React from 'react';
+import React, { use } from 'react';
 import { useRouter } from 'next/navigation';
 import PromotionFormModal from '@/app/components/promotion-form-modal';
 
 export interface PageProps {
-  params: { id: string | Promise<string> };
+  params: Promise<{ id: string }>;
 }
 
 export default function Page({ params }: PageProps) {
   const router = useRouter();
 
-  const id = typeof params.id === 'string' ? params.id : React.use(params.id);
+  const { id } = use(params);
 
   return (
     <PromotionFormModal
--- src/app/(admin)/companies/[id]/new-promotion/page.tsx
+++ src/app/(admin)/companies/[id]/new-promotion/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import React from 'react';
+import React, { use } from 'react';
 import PromotionForm from '@/app/components/promotion-form';
 
 export interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default function Page({ params }: PageProps) {
+  const { id } = use(params);
+
   return (
     <div className="py-6 px-10">
-      <PromotionForm companyId={params.id} />
+      <PromotionForm companyId={id} />
     </div>
   );
 }
